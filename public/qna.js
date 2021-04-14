12365daefb44aea0bd77b76062c31460aab3c8db
kmkim <kkgg7527@gmail.com>
2021-04-14 23:46:00 +0900

    add: fetch API & promise/then 비동기 제어를 통한 질문 데이터 조회.

--- public/qna.js
+++ public/qna.js
@@ -30,24 +30,49 @@ function getQnATemplate(data) {
 		return (
 			html +
 			` <li class="qna" _questionId=${+id}>
-        <div class="qna-title">
-            <h2>${title}</h2>
-        </div>
-        <div class="question">
-            <p> ${question}</p>
-        </div>
-        <ul class="answer">${getAnswerTemplate(matchedComments)}</ul>
-        <div class="answer-form">
-            <form method="POST">
-                <textarea name="answer-content" class="answer-content-textarea" cols="30" rows="2" placeholder="새로운답변.."></textarea>
-            </form>
-            <button class="answer-submit">등록</button>
-        </div>
-    </li>`
+				<div class="qna-title">
+					<h2>${title}</h2>
+				</div>
+				<div class="question">
+					<p> ${question}</p>
+				</div>
+				<ul class="answer">${getAnswerTemplate(matchedComments)}</ul>
+				<div class="answer-form">
+					<form method="POST">
+						<textarea name="answer-content" class="answer-content-textarea" cols="30" rows="2" placeholder="새로운답변.."></textarea>
+					</form>
+					<button class="answer-submit">등록</button>
+				</div>
+			</li>`
 		);
 	}, ``);
 }
 
+function getData (url) {
+	return new Promise(function(resolve, reject) {
+		setTimeout(() => {
+
+			fetch(url)
+				.then(response => response.json())
+				.then(data => {
+					resolve(data);
+				}
+
+				)
+				.catch(error => console.log(error));
+		}, 500);
+	});
+}
+
+function makeQuestionList () {
+	getData(URL.questions).then( data => {
+		document.querySelector(".qna-wrap").innerHTML = getQnATemplate(data);
+	});
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 	//코드시작
+	
+	makeQuestionList();
+
 });
