a9b0d84a47d333c3994134598a84616419c5f327
domuk-k <dannyworks102@gmail.com>
2021-04-15 15:20:55 +0900

    feat: util funs isolation

--- public/utils.js
+++ public/utils.js
@@ -6,3 +6,34 @@ export function errorCatcher(fn) {
     console.error(error);
   }
 }
+
+/**
+ * @namespace QnAUtils utility functions
+ */
+export default {
+  /**
+   * @deprecated
+   */
+  reduceArrayObject(array, key) {
+    return array.reduce((result, item) => {
+      const objectKey = item[key];
+      return {
+        ...result,
+        [objectKey]: result[objectKey] ? [...result[objectKey], item] : [item],
+      };
+    }, {});
+  },
+  /**
+   * @description simple implementaion of pipe function, receives multiple functions and execute consecutively
+   */
+  pipe(...functions) {
+    return (arg) => functions.reduce((result, func) => func(result), arg);
+  },
+
+  processFormToObject(arg) {
+    return this.pipe(
+      (formElement) => new FormData(formElement),
+      Object.fromEntries
+    )(arg);
+  },
+};
