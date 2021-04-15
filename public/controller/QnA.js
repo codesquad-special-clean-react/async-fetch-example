b6f75ee00ecfbcd3e651cfe76c0a08a7ccaf2d4a
domuk-k <dannyworks102@gmail.com>
2021-04-15 16:40:11 +0900

    fix: remove comments

--- public/controller/QnA.js
+++ public/controller/QnA.js
@@ -14,11 +14,6 @@ class QnA {
     this.addListeners();
   }
 
-  // subscription은 모델이 가져야할 메서드가 맞는거같은데,,
-  // 그럼 데이터를 수정하는 건 controller, model? 누가 제공해야하나.
-  // 그것도 일일이 메서드로 만들어야하나
-  // dispatch... flux 패턴이 이래서 필요한것이엇나,,난 OOP도 FP도 몰라,,,
-
   onQuestionSubmit = async (e) => {
     e.preventDefault();
     const { target: $form } = e;
@@ -37,11 +32,7 @@ class QnA {
     const questionId = +e.target.closest('li').getAttribute('_questionid');
     const date = new Date().toISOString().slice(0, 10);
 
-    // loading
-    // this.view.getLoadingAnswerTpl();
     await this.model.addAnswer({ content, questionId, date });
-    // end loading
-    // maybe by state mgmt?
   };
 
   onClose = (e) => {
