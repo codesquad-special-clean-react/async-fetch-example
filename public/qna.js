ecad4a313fe5f45ba14849b899d4ccfcbc55da5f
HHJ-S2 <berdle23@knou.ac.kr>
2021-04-17 12:44:07 +0900

    Add: fetch 함수를 사용하여 데이터 호출

--- public/qna.js
+++ public/qna.js
@@ -1,13 +1,13 @@
 const URL = {
-	questions: 'http://localhost:3001/questions',
-	answers: 'http://localhost:3001/answers',
+  questions: "http://localhost:3001/questions",
+  answers: "http://localhost:3001/answers",
 };
 
 function getAnswerTemplate(answers) {
-	return answers.reduce((html, { content, userId, date }) => {
-		return (
-			html +
-			`
+  return answers.reduce((html, { content, userId, date }) => {
+    return (
+      html +
+      `
         <li class="answer-list" ">
             <p class="answer-content">${content}</p>
             <div class="answer-profile">
@@ -15,21 +15,21 @@ function getAnswerTemplate(answers) {
                 <span class="answer-date">${date}</span>
             </div>
         </li>`
-		);
-	}, ``);
+    );
+  }, ``);
 }
 
 function getLoadingAnswerTpl() {
-	return `<li class="answer-list loading" ">
+  return `<li class="answer-list loading" ">
         Loading.....
      </li>`;
 }
 
 function getQnATemplate(data) {
-	return data.reduce((html, { title, question, id, matchedComments = [] }) => {
-		return (
-			html +
-			` <li class="qna" _questionId=${+id}>
+  return data.reduce((html, { title, question, id, matchedComments = [] }) => {
+    return (
+      html +
+      ` <li class="qna" _questionId=${+id}>
         <div class="qna-title">
             <h2>${title}</h2>
         </div>
@@ -44,10 +44,34 @@ function getQnATemplate(data) {
             <button class="answer-submit">등록</button>
         </div>
     </li>`
-		);
-	}, ``);
+    );
+  }, ``);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-	//코드시작
+document.addEventListener("DOMContentLoaded", () => {
+  async function getQuestionData() {
+    try {
+      const data = await fetch(URL.questions);
+      const questions = await data.json();
+
+      return questions;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function getAnswersData() {
+    try {
+      const data = await fetch(URL.answers);
+      const answers = await data.json();
+
+      console.log(answers);
+      return answers;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  getQuestionData();
+  getAnswersData();
 });
