8175493c586b106b7f7138e5aba942c8036eb7b3
domuk-k <dannyworks102@gmail.com>
2021-05-08 17:26:50 +0900

    init: 혼자 하는 리액트워크샵

--- public/constants.js
+++
@@ -1,4 +0,0 @@
-export const URL = {
-  questions: 'http://localhost:3001/questions',
-  answers: 'http://localhost:3001/answers',
-};
--- public/index.js
+++
@@ -1,5 +0,0 @@
-import QnA from './controller/QnA.js';
-
-document.addEventListener('DOMContentLoaded', async () => {
-  new QnA();
-});
