fae275652956ad0702ad303aac6de7e1397b14c1
domuk-k <dannyworks102@gmail.com>
2021-05-08 17:54:29 +0900

    style: basic set ups for code style

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'no-var': 'warn',
+    'no-undef': 0,
+    'no-console': 'warn',
+  },
+};
