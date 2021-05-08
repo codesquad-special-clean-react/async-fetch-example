82ad94464fbdb59a705d9b7d3bab1c98b8cbc2d3
domuk-k <dannyworks102@gmail.com>
2021-05-08 18:00:31 +0900

    config: eslint parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
   env: {
     browser: true,
+    es2021: true,
   },
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
   rules: {
     'no-var': 'warn',
     'no-undef': 0,
